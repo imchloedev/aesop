174158f3adea86ff52dc1e1f3645c373cd3db1fd
kang-eunji <70960594+kang-eunji@users.noreply.github.com>
2022-06-09 13:56:04 +0900

    style rearranged

---
+++ js/cart.js
@@ -0,0 +1,29 @@
+// quantity
+
+function count(type) {
+  // 결과를 표시할 element
+  const resultElement = document.getElementById('result');
+  const resultAdultsElement = document.getElementById('adults_result');
+
+  // const reset = document.getElementById('resetbtn');
+
+  // 현재 화면에 표시된 값
+  let number = resultElement.innerText;
+
+  // 더하기/빼기
+  if (type === 'plus') {
+    if (number !== 0) {
+      number = parseInt(number) + 1;
+    };
+  } else if (type === 'minus') {
+    if (number <= 0) {
+      number = parseInt(number) - 0;
+    } else {
+      number = parseInt(number) - 1;
+    };
+  };
+
+  // 결과 출력
+  resultElement.innerText = number;
+  resultAdultsElement.innerText = number;
+};
